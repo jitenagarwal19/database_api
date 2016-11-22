3f1ef410ad2fbe2c30b31621711763ee9df57ed0
jitenagarwal <jitenagarwal19@gmail.com>
2016-11-22 06:31:41 +0530

    adding urls to store indices

--- nse_api/urls.py
+++ nse_api/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     url(r'^get_content_indices$', views.get_contents_indices, name='get_contents_indices'),
     url(r'^read_data_from_local$', views.read_data_from_local, name='read_data_from_local'),
     url(r'^save_data_from_local$', views.save_data_from_local, name='save_data_from_local'),
+    url(r'^create_indices_table$', views.create_table_to_store_indices, name='create_table_to_store_indices'),
+    url(r'^store_indices$', views.store_value_in_table, name='store_value_in_table'),
     url(r'^nse_ind/([a-z0-9]{1,20})/start-date/([0-9]{4}-[0-9]{2}-[0-9]{2})/end-date/([0-9]{4}-[0-9]{2}-[0-9]{2})/$', views.is_present, name='is_present'),
 ]
 
